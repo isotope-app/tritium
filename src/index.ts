f9f43037c9f79a00023872f3fda0cc319aed0e2f
ivy <ivy@not-ivy.dev>
2022-12-24 23:49:48 -0800

    server only

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { program } from 'commander';
-import Client from './client/index.js';
-import Server from './server/index.js';
+import { WebSocketServer } from 'ws';
 
-program.option('--client').option('--server');
+import minimist from 'minimist';
 
-program.parse();
+const args = minimist(process.argv.slice(2));
+const port = args.port || process.env.PORT || 8080;
 
-const options = program.opts();
+const wss = new WebSocketServer({ port });
+wss.on('connection', (conn) => {
+  conn.send(`Tritium v${process.env.npm_package_version}`);
 
-if (options.client) {
-  Client.run();
-} else if (options.server) {
-  Server.run();
-}
+  conn.on('message', (message) => {
+    console.log(`Received message => ${message}`);
+  });
+});
+
+wss.on('listening', () => {
+  console.log(`Tritium is listening on port ${port}`);
+});
